a977fb75d35c713d9a49d36141f355d05ad8945b
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2025-10-24 13:56:56 +0700

    update frontend

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import AnimasiTeks from '../components/AnimasiTeks'
-import Footer from '../components/Footer'
+import React, { useEffect } from 'react';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import AnimasiTeks from '../components/AnimasiTeks';
+import Footer from '../components/Footer';
+import { getHello } from '../Api';
 
 const User = () => {
-  return (
-    <div className='h-screen overflow-auto '>
-        <Header/>
-        <Hero/>
-        <AnimasiTeks/>
-        <Footer/>
-    </div>
-  )
-}
 
-export default User
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getHello();
+      console.log("Message from backend:", data.message); // <-- log di console
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className='h-screen overflow-auto'>
+      <Header/>
+      <Hero/>
+      <AnimasiTeks/>
+      <Footer/>
+    </div>
+  );
+};
+
+export default User;
