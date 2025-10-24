067fb238124698efc43ca56d286dd137a7598f56
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2025-10-24 14:26:15 +0700

    update frontend 3

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = "https://backendmakeover-production.up.railway.app/"; // Gunakan domain Anda
+const API_URL = "https://backendmakeover-production.up.railway.app"; // HAPUS slash di akhir
 
 export const getHello = async () => {
   try {
-    const response = await axios.get(`${API_URL}/api/hello`);
+    const response = await axios.get(`${API_URL}/api/hello`); // slash hanya di sini
     return response.data;
   } catch (error) {
     console.error("Error fetching data:", error);
