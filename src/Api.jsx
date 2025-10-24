529ec5d71c731ffc658381febc4f51722aecb272
baihaqie <baihaqiearrafi@students.polmed.ac.id>
2025-10-24 14:20:54 +0700

    update frontend 2

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://backend-makeover.com"; // Gunakan domain Anda
+const API_URL = "https://backendmakeover-production.up.railway.app/"; // Gunakan domain Anda
 
 export const getHello = async () => {
   try {
